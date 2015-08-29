6b0f3a3812a8fa58dd3b789c19f214f6ebb2b8f0
kolbeypruitt <kolbeypruitt@gmail.com>
2015-08-29 15:27:56 -0600

    main layout complete

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Hipstafy',
+    weWill: "Write a sentence or two, and we'll hipstafy it for you" });
 });
+
+
+router.post('/foo/bar', function (req, res, next) {
+  if (req.body == {emotion: "happy", level: "very"}) {
+    res.render('index', {title: "No way! That worked?"});
+  }
+})
 
 module.exports = router;
